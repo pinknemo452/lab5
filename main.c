f35030a72c97ebf2e63ec0920f66a57b9509236e
pinknemo452 <mishanay38@gmail.com>
2020-11-02 14:32:26 +0300

    peredelal

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int onlychars(char* word,int size) {
 	for (int i = 0; i < size; i++) {
 		//printf(" %c ", word[i]);
 
-		if( (word[i]<65) || ((word[i] > 65) && (word[i] < 90)) || ((word[i] > 90) && (word[i] < 97)) || (word[i] > 122)){
+		if( (word[i]<'A') || ((word[i] > 'Z') && (word[i] < 'a')) || (word[i] > 'z') ){
 			return 0;
 		}
 	}
